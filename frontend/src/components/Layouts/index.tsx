c7e22892837084b25eba3a2aedacb41d3647d08d
Ruan Medeiros <pm.ruan@gmail.com>
2023-08-09 16:01:14 -0300

    refactor: page layout align items center

--- frontend/src/components/Layouts/index.tsx
+++ frontend/src/components/Layouts/index.tsx
@@ -6,10 +6,10 @@ export function Layout(): JSX.Element {
     <div className="p-16 flex h-screen bg-gray-950 text-slate-500 font-fira justify-center">
       <div
         id="container"
-        className="flex flex-col w-[800px] h-[calc(100vh-8rem)] bg-slate-900 border border-slate-800 rounded-lg"
+        className="flex flex-col w-fit h-[calc(100vh-8rem)] bg-slate-900 border border-slate-800 rounded-lg"
       >
         <Header author="ruan-pablo-medeiros" />
-        <div className="h-[calc(100%-3.8rem-2.8rem)]">
+        <div className="flex flex-col h-[calc(100%-3.8rem-2.8rem)] p-8 items-center">
           <Outlet />
         </div>
         <Footer />
