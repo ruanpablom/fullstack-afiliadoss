99b9cdd98e6524adeada5ec47f1671cf6f250d23
Ruan Medeiros <pm.ruan@gmail.com>
2023-08-09 16:08:54 -0300

    chore: add font fira code

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fira: ['Fira Code'],
+      },
+    },
   },
   plugins: [],
 };
