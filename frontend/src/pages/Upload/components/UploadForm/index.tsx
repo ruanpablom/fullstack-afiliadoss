5e9ff1d8b022655ad809cafaa9b0885a931469a6
Ruan Medeiros <pm.ruan@gmail.com>
2023-08-09 17:04:01 -0300

    refactor: add loading spinner to send button

--- frontend/src/pages/Upload/components/UploadForm/index.tsx
+++ frontend/src/pages/Upload/components/UploadForm/index.tsx
@@ -37,14 +37,14 @@ export function UploadForm(): JSX.Element {
         errors={errors.file}
       />
       <button
-        className=" bg-indigo-500 text-slate-900 font-semibold px-2 py-1 rounded-sm cursor-pointer hover:bg-indigo-400"
+        className="flex items-center justify-center bg-indigo-500 text-slate-900 font-semibold px-2 py-1 rounded-sm cursor-pointer hover:bg-indigo-400"
         type="submit"
       >
         {loading ? (
-          <>
+          <div className="w-fit">
             <svg
               aria-hidden="true"
-              className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              className="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-emerald-400"
               viewBox="0 0 100 101"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -59,7 +59,7 @@ export function UploadForm(): JSX.Element {
               />
             </svg>
             <span className="sr-only">Loading...</span>
-          </>
+          </div>
         ) : (
           `SEND`
         )}
